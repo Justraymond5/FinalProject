2df48217fec2464cf5f1f01085a755ff3e74a6fb
raymondgonzalez <collegeeduraymond@gmail.com>
2025-03-02 16:39:28 -0800

    Made the menu a method to call to make the code more clear.

--- Final Project - Library System/Program.cs
+++ Final Project - Library System/Program.cs
@@ -1,6 +1,20 @@
 ﻿namespace Final_Project___Library_System;
 class Program
 {
+    /// <summary>
+    /// Method for holding the prompt
+    /// </summary>
+    static void MainMenu()
+    {
+        Console.WriteLine("\nPlease choose an option:");
+        Console.WriteLine("1. Add a Book");
+        Console.WriteLine("2. Add a Movie");
+        Console.WriteLine("3. Display All Items");
+        Console.WriteLine("4. Edit an Item");
+        Console.WriteLine("5. Exit");
+        Console.Write("\nYour choice: ");
+        
+    }
     static void Main()
     {
         //Ensure that I can add 50+ items
@@ -9,13 +23,13 @@ class Program
         
         while (true)
         {
-            Console.WriteLine("\nPlease choose an option:");
-            Console.WriteLine("1. Add a Book");
-            Console.WriteLine("2. Add a Movie");
-            Console.WriteLine("3. Display All Items");
-            Console.WriteLine("4. Edit an Item");
-            Console.WriteLine("5. Exit");
-            Console.Write("\nYour choice: ");
+            MainMenu();
+            int choice;
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 5.");
+                continue;
+            }
 
             
         }
