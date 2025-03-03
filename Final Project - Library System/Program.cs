91775cb4cf7c0a152b7361f944f40320d65dde45
raymondgonzalez <collegeeduraymond@gmail.com>
2025-03-02 16:27:46 -0800

    Added menu in program

--- Final Project - Library System/Program.cs
+++ Final Project - Library System/Program.cs
@@ -1,9 +1,23 @@
 ﻿namespace Final_Project___Library_System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        //Ensure that I can add 50+ items
+        List<CheckOut> libraryItems = new();
+        Console.WriteLine("Welcome to the Library Management System!");
+        
+        while (true)
+        {
+            Console.WriteLine("\nPlease choose an option:");
+            Console.WriteLine("1. Add a Book");
+            Console.WriteLine("2. Add a Movie");
+            Console.WriteLine("3. Display All Items");
+            Console.WriteLine("4. Edit an Item");
+            Console.WriteLine("5. Exit");
+            Console.Write("\nYour choice: ");
+
+            
+        }
     }
 }
