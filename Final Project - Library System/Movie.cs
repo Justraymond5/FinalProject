69f0e15e9a7fbcb184e856d180e4757f9175dea3
raymondgonzalez <collegeeduraymond@gmail.com>
2025-03-02 16:04:00 -0800

    Added a Movie class inherinitng from : CheckOut

---
+++ Final Project - Library System/Movie.cs
@@ -0,0 +1,20 @@
+namespace Final_Project___Library_System;
+/// <summary>
+/// Child class inheriting from the general CheckOut abstract class
+/// </summary>
+public class Movie : CheckOut
+{
+    public string Director { get; set; }
+
+    public Movie(string title, int time, int year, string director) : base(title, time, year)
+    {
+        Director = director;
+    }
+    /// <summary>
+    /// This will override the class to display everything in the parent class with individual child properties
+    /// </summary>
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Movie: {Title}, Director: {Director}, Check Out Time (in weeks): {Time} weeks, Year: {Year}");
+    }
+}
